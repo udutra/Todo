c61a751167dfccad59fef8d7daf91b1cdc9ba35f
Guilherme Galarça Dutra <udutra@gmail.com>
2024-02-19 19:37:23 -0300

    Lendo itens do banco de dados Criando um registro Atualizando e excluindo um registro Testando a API Melhorando a API

--- obj/Debug/net8.0/Todo.AssemblyInfo.cs
+++ obj/Debug/net8.0/Todo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Todo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+743b003d8a6091fbae1459b978882fabfee98871")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f983357f39d4ace109e1072d0b7bd759cdcd4a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Todo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Todo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

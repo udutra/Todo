2f983357f39d4ace109e1072d0b7bd759cdcd4a4
Guilherme Galarça Dutra <udutra@gmail.com>
2024-02-19 19:06:41 -0300

    Gerando o banco de dados Entendendo o padrão MVC Entendendo os Controllers Rotas e Controllers Adicionando suporte a Controllers

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Todo.Controllers;
+
+[ApiController]
+public class HomeController: ControllerBase
+{
+    [HttpGet("/a")]
+    public string Get()
+    {
+        return "Hello World!";
+    }
+}
--- obj/Debug/net8.0/Todo.AssemblyInfo.cs
+++ obj/Debug/net8.0/Todo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Todo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8c0109647d1fe335e3303853845583e04760d17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+743b003d8a6091fbae1459b978882fabfee98871")]
 [assembly: System.Reflection.AssemblyProductAttribute("Todo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Todo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
